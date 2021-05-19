330a4a55afd193ee4a52d1b6c8b48d4e0e6618ad
Katie Hyewon Choi <hyewonchoi31@gmail.com>
2021-05-18 21:11:54 -0400

    commit tree 19b9ec4891633cfa1f03aee540a5f484f7120db8

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,18 +3,26 @@ require 'pry'
 class String
 
   def sentence?
-    
+    self.end_with?(".")
   end
 
   def question?
-
+    self.end_with?("?")
   end
 
   def exclamation?
+    self.end_with?("!")
 
   end
 
   def count_sentences
-
+    # sent_array = self.split(" ").reject { |str| str.empty? }
+    # sent_array.count
+    
+    self.split(/\.|\?|\!/).delete_if {|w| w.size < 2}.size
+   
+    # binding.pry 
   end
+  # binding.pry #does not work? , count_sentences // error
 end
+# binding.pry #does not work?count_sentences // error
